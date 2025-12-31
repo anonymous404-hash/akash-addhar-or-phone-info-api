643c50ec53525cc98bcc749f6edfa6bf95096fd6
ANONYMOUS-HACKERS <metrom6x1@gmail.com>
2025-12-31 12:37:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 
-const JSON_URL = "YOUR_JSON_DIRECT_DOWNLOAD_LINK";
+const JSON_URL = "https://github.com/anonymous404-hash/Addharinfoapi/releases/download/v1.0/database.json";
 
 app.get("/", (req, res) => {
   res.json({ status: true, message: "API Working 🎉" });
